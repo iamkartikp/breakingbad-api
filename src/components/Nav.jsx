c8a05d3b55facbcdc6e8772661ba5b12d202a7ae
Kartik <55845219+iamkartikp@users.noreply.github.com>
2020-07-10 18:27:46 +0530

    Fixed Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,25 @@ class Nav extends Component {
     componentDidMount() {
         const nav = document.querySelector('.nav');
         const navLinks = document.querySelector('.nav-links-ul');
+        const navLinksLi = document.querySelector('.nav-links-ul li');
+        const oneId = document.querySelector('#one');
+        const twoId = document.querySelector('#two');
+        const threeId = document.querySelector('#three');
 
         nav.addEventListener('click', () =>{
             navLinks.classList.toggle('nav-active');
+        })
+        navLinksLi.addEventListener('click', () => {
+            navLinks.classList.remove('nav-active');
+        })
+        oneId.addEventListener('click', () => {
+            navLinks.classList.remove('nav-active');
+        })
+        twoId.addEventListener('click', () => {
+            navLinks.classList.remove('nav-active');
+        })
+        threeId.addEventListener('click', () => {
+            navLinks.classList.remove('nav-active');
         })
     }
     render() {
@@ -22,9 +38,9 @@ class Nav extends Component {
                     <div className="nav-links">
                         <ul className="nav-links-ul">
                             <li><Link to="/">Home</Link> </li>
-                            <li><NavLink to="/episodes">Episodes</NavLink></li>
-                            <li><NavLink to="/quotes">Quotes</NavLink></li>
-                            <li><NavLink to="/deaths">Deaths</NavLink></li>
+                            <li><NavLink id="one" to="/episodes">Episodes</NavLink></li>
+                            <li><NavLink id="two" to="/quotes">Quotes</NavLink></li>
+                            <li><NavLink id="three" to="/deaths">Deaths</NavLink></li>
                         </ul>
                     </div>
             </React.Fragment>
